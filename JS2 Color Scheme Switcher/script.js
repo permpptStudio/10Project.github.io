3ba50de44801fe6ac6250a0e5a79731e011b2c22
permpptStudio <122204000+permpptStudio@users.noreply.github.com>
2023-01-15 18:43:41 +0700

    Initial commit

---
+++ JS2 Color Scheme Switcher/script.js
@@ -0,0 +1,28 @@
+document.getElementById('grayButton').onclick = switchGray;
+document.getElementById('whiteButton').onclick = switchWhite;
+document.getElementById('blueButton').onclick = switchBlue;
+document.getElementById('yellowButton').onclick = switchYellow;
+
+function switchGray() {
+    document.getElementsByTagName('body')[0] .style.backgroundColor ='gray';
+    document.getElementsByTagName('body')[0] .style.color ='white';
+
+}
+
+function switchWhite() {
+    document.getElementsByTagName('body')[0] .style.backgroundColor ='White';
+    document.getElementsByTagName('body')[0] .style.color ='black';
+
+}
+
+function switchBlue() {
+    document.getElementsByTagName('body')[0] .style.backgroundColor ='Blue';
+    document.getElementsByTagName('body')[0] .style.color ='white';
+
+}
+
+function switchYellow() {
+    document.getElementsByTagName('body')[0] .style.backgroundColor ='yellow';
+    document.getElementsByTagName('body')[0] .style.color ='black';
+
+}
